4ed07b2f5fe8c048c415d5d8ec1d5cc5b0fca4b3
Emir Öztürk <62071518+emirozturk1@users.noreply.github.com>
2021-01-22 16:19:40 +0300

    Add files via upload

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryList<string, string> kisiler = new MyDictionaryList<string, string>();
+            kisiler.Add("177", "Emir");
+            kisiler.Add("158", "Emre");
+
+            foreach (var boy in kisiler.Keys)
+            {
+                Console.WriteLine(boy);
+            }
+
+            foreach (var isim in kisiler.Values)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
