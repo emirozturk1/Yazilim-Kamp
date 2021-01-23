0ab8c0a58f0483bfe488616c4c18c60e94cb6c32
Emir Öztürk <62071518+emirozturk1@users.noreply.github.com>
2021-01-23 23:54:22 +0300

    OOP2 5.Gün

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri()
+            {
+                MusteriNo = "123",
+                Adi = "Enes",
+                Soyadi = "Yılmaz",
+                TcNo = "123123123",
+                Id = 1
+            };
+
+            TuzelMusteri musteri2 = new TuzelMusteri()
+            {
+                Id = 2,
+                MusteriNo = "134",
+                SirketAdi = "Rada",
+                VergiNo = "11523123",
+            };
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+        }
+    }
+}
