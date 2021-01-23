6ff3f02f2be010e5cf8ebbbfcdf82ac92fde722a
Emir Öztürk <62071518+emirozturk1@users.noreply.github.com>
2021-01-23 23:54:58 +0300

    OOP3 5.Gün

---
+++ OOP3/FileLoggerService.cs
@@ -0,0 +1,11 @@
+﻿using OOP3;
+using System;
+
+class FileLoggerService : ILoggerService
+    {
+    public void Log()
+    {
+        Console.WriteLine("Dosyaya loglandı");
+
+    }
+}
---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //farklı/alternatif türler farklı/alternatif hesaplamları yapmak içinn interface kullanılır.
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+    }
+}
